b7251f9d20f4794c2a21f96bc8a9800b0368ae4c
Ananasy <anastasiya_johnni@mail.ru>
2018-11-13 09:20:58 +0300

    sintezzz initial

---
+++ sint/laba_one/main.py
@@ -0,0 +1,35 @@
+import os
+import logging
+from sint.laba_one.dataset import Dataset
+
+from task_2.classifiers.XGBOOST import XGBOOST
+
+logger = logging.getLogger('laba_one')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+root_path = '../../data/'
+file_name = 'subparts.xml'
+ds = Dataset(os.path.join(root_path, file_name), 14)
+ds.create_ds()
+
+logger.info(msg='Dataset loaded!')
+
+x, y = ds.train_set['data'], ds.train_set['labels']
+x_test, y_test = ds.test_set['data'], ds.test_set['labels']
+num_train_ex, feat_len = x.shape
+
+logger.info(msg='Size of train set: {}, size of features in train set: {}'.format(num_train_ex, feat_len))
+
+xgboostEngine = XGBOOST()
+xgboostEngine.fit(x, y)
+
+score = xgboostEngine.check_accuracy(x_test, y_test)
+
+logger.info(msg='Score on test set: {}'.format(score))
+
