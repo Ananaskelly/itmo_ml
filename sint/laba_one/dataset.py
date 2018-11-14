107647333517bc361a30b8ba29ea4eb1aff55bf3
Ananasy <anastasiya_johnni@mail.ru>
2018-11-14 17:54:34 +0300

    up

--- sint/laba_one/dataset.py
+++ sint/laba_one/dataset.py
@@ -1,33 +1,12 @@
 import numpy as np
 from xml.etree import ElementTree
+from sint.base_dataset import BaseDataset
 
 
-class Dataset:
+class Dataset(BaseDataset):
 
     def __init__(self, path_to_in_xml, valid_part=0.1, test_part=0.1):
-        self._path_to_in_xml = path_to_in_xml
-
-        self._test_set = {}
-        self._valid_set = {}
-        self._train_set = {}
-
-        self._valid_part = valid_part
-        self._test_part = test_part
-
-        self._sym_dict = {':': 1, ',': 2, '.': 3, '!': 4, '?': 5}
-        self._sym_keys = self._sym_dict.keys()
-
-    @property
-    def test_set(self):
-        return self._test_set
-
-    @property
-    def valid_set(self):
-        return self._valid_set
-
-    @property
-    def train_set(self):
-        return self._train_set
+        super(Dataset, self).__init__(path_to_in_xml, valid_part=valid_part, test_part=test_part)
 
     def parse_xml(self):
         tree_root = ElementTree.parse(self._path_to_in_xml).getroot()
